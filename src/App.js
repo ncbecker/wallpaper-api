968a7e92c86246aea5d47209fda97a2d4c76183d
ncbecker <nbecker.ac@gmail.com>
2020-10-27 16:02:27 +0100

    :sparkles: Create image preview component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import ImagePreview from "./ImagePreview";
 
 function App() {
-  return <></>;
+  return (
+    <main>
+      <button>GET RANDOM IMAGE</button>
+      <ImagePreview
+        src={
+          "https://images.unsplash.com/photo-1603726964374-b9f6e020f0e7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1834&q=80"
+        }
+        alt={"Surfer"}
+        author={"Unsplash"}
+      />
+    </main>
+  );
 }
 
 export default App;
