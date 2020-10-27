3b21c96bcfc9f1189eaafba8e72fa7693f1cd72d
ncbecker <nbecker.ac@gmail.com>
2020-10-27 18:11:18 +0100

    Add Unsplash API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
+import { getRandomImage } from "./api/getRandomImage";
 import "./App.css";
 import Button from "./Button";
 import ImagePreview from "./ImagePreview";
 
 function App() {
+  // const randomImageInfo = async function loadRandomImage(url) {
+  //   const randomImage = await getRandomImage;
+  //   return randomImage;
+  // };
+
+  /*randomImageInfo.urls.thumb*/
+
   return (
     <main>
       <Button innerText={"GET RANDOM IMAGE"} />
       <ImagePreview
         src={
-          "https://images.unsplash.com/photo-1603726964374-b9f6e020f0e7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1834&q=80"
+          "https://images.unsplash.com/photo-1526485856375-9110812fbf35?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80"
         }
         alt={"Surfer"}
         author={"Unsplash"}
