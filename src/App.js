08dce2187f4e5f68a33e5d67b45307a9dea2df87
ncbecker <nbecker.ac@gmail.com>
2020-10-27 16:39:00 +0100

    Add button component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
+import Button from "./Button";
 import ImagePreview from "./ImagePreview";
 
 function App() {
   return (
     <main>
-      <button>GET RANDOM IMAGE</button>
+      <Button innerText={"GET RANDOM IMAGE"} />
       <ImagePreview
         src={
           "https://images.unsplash.com/photo-1603726964374-b9f6e020f0e7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1834&q=80"
---
+++ src/Button.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Button.css";
+
+export default function Button({ innerText }) {
+  return <button>{innerText}</button>;
+}
