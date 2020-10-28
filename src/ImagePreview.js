78a8eaa85d933242470cb0a6c997e67830cb2cac
ncbecker <nbecker.ac@gmail.com>
2020-10-28 12:13:55 +0100

    Create favorites button

--- src/ImagePreview.js
+++ src/ImagePreview.js
@@ -4,6 +4,14 @@ import "./ImagePreview.css";
 export default function ImagePreview({ src, alt, author }) {
   return (
     <div className="imageContainer">
+      <button
+        className="btn__favorites"
+        onClick={() => {
+          alert("It works");
+        }}
+      >
+        ♡
+      </button>
       <img className="imageThumb" src={src} alt={alt} author={author} />
       <p className="imageAuthor">Author: {author}</p>
     </div>
